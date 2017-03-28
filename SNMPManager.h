e381b1beee40f5b3abf110ed32093b9731f13337
OGMalin <oddg@sjakk.biz>
2017-03-28 14:22:57 +0200

    set default write community to private

--- SNMPManager.h
+++ SNMPManager.h
@@ -38,7 +38,7 @@ public:
 	void stop();
 	bool read(SNMPTelegram& t);
 	void getRequest(DWORD ip, std::string object, std::string value="NULL", int version=1, std::string community="public");
-	void setRequest(DWORD ip, std::string object, std::string value = "NULL", int version = 1, std::string community = "public");
+	void setRequest(DWORD ip, std::string object, std::string value = "NULL", int version = 1, std::string community = "private");
 	void send(DWORD ip, BYTE* data, int len);
 	static void trapLoop(void* lpv);
 	static void responseLoop(void* lpv);
